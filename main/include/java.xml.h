eaec6b0d7b1d3c362e3b9a226d165017e5c81d58
Yao Yuan <yeaya@163.com>
2025-12-29 18:48:49 +0800

    add  dependency info in lib include file, etc.

--- main/include/java.xml.h
+++ main/include/java.xml.h
@@ -1,5 +1,6 @@
 #ifndef _java$xml_
 #define _java$xml_
+//$ dependency java.base:17.35
 
 #include <java/lang/def.h>
 
--- test/classes/java.xml.test.h
+++ test/classes/java.xml.test.h
@@ -1,5 +1,7 @@
 #ifndef _java$xml$test_
 #define _java$xml$test_
+//$ dependency java.xml:17.35
+//$ dependency java.base:17.35
 
 #include <java/lang/def.h>
 
